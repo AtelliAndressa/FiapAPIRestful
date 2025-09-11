83531a40d1fb903b86db10c090da8f33d4fa365f
Andressa Atelli <andressaatelli.programador@gmail.com>
2025-09-11 19:36:03 -0300

    Adicionar arquivos de projeto.

---
+++ src/Core.Domain/Entities/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Entities
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
