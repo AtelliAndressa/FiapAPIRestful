88b4a4eb1fa3ef37862fbceda924c14bb20594ba
Andressa Atelli <andressaatelli.programador@gmail.com>
2025-09-11 23:30:09 -0300

    implementação do authController para a segurança

---
+++ src/API/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Realiza o login de um usuário válido e gera um token JWT.
+    /// </summary>
+    /// <remarks>
+    /// Este método valida as credenciais fornecidas (usuário e senha) e, caso sejam válidas,
+    /// retorna um token JWT que poderá ser utilizado para acessar recursos protegidos da API.
+    /// 
+    /// O token gerado expira em 3 horas a partir da autenticação.
+    /// </remarks>
+    /// <param name="model">Modelo contendo o nome de usuário e a senha para autenticação.</param>
+    /// <returns>
+    /// Um <see cref="IActionResult"/> contendo o token JWT em caso de sucesso;
+    /// caso contrário, retorna <see cref="UnauthorizedResult"/>.
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new(ClaimTypes.Name, user.UserName),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        return Unauthorized();
+    }
+}
+
+public record LoginModel(string Username, string Password);
