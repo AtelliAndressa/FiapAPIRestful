49e9afe8d76ab914450d49e203d0440c0259cdfd
Andressa Atelli <andressaatelli.programador@gmail.com>
2025-09-24 10:25:52 -0300

    implementando validadores para senha

---
+++ src/Core.Application/Validators/ChangePasswordDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using Core.Application.DTOs;
+using FluentValidation;
+
+namespace Core.Application.Validators
+{
+    public class ChangePasswordDtoValidator : AbstractValidator<ChangePasswordDto>
+    {
+        public ChangePasswordDtoValidator()
+        {
+            RuleFor(x => x.NewPassword).NotEmpty();
+            RuleFor(x => x.ConfirmNewPassword)
+                .Equal(x => x.NewPassword).WithMessage("A nova senha e a confirmação não conferem.");
+        }
+    }
+}
---
+++ src/Core.Application/Validators/ResetPasswordDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Core.Application.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Validators
+{
+    public class ResetPasswordDtoValidator : AbstractValidator<ChangePasswordDto>
+    {
+        public ResetPasswordDtoValidator()
+        {
+            RuleFor(x => x.NewPassword).NotEmpty();
+            RuleFor(x => x.ConfirmNewPassword)
+                .Equal(x => x.NewPassword).WithMessage("A nova senha e a confirmação não conferem.");
+        }
+    }
+}
